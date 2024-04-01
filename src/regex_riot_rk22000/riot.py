f9c6fcc73e80aa052bdebb3b1fe12e37a5a7c93f
Rahul <rahukand@gmail.com>
2024-04-01 15:10:46 -0700

    Built it enough to solve the first exercise

--- src/regex_riot_rk22000/riot.py
+++ src/regex_riot_rk22000/riot.py
@@ -4,10 +4,13 @@ this module.
 
 """
 
+import re
+
 class RiotString:
-    def __init__(self, re:str) -> None:
-        self._a = re
+    def __init__(self, re, unit=False) -> None:
+        self._a = str(re)
         self._b = ""
+        self._unit = unit
 
     @property
     def a(self):
@@ -15,20 +18,41 @@ class RiotString:
     @property
     def b(self):
         return self._b
+    @property
+    def unit(self):
+        return self._unit
 
     def __str__(self) -> str:
         return self.a + self.b
+    
+    def then(self, rs):
+        return RiotString(str(self)+str(rs))
+    def one_or_more(self):
+        if self.unit:
+            return RiotString(f"{self.a}+")
+        else:
+            return RiotString(f"({self.a})+")
+    
+    def compile(self) -> re.Pattern:
+        return re.compile(str(self))
 
-    def __eq__(self, __value: object) -> bool:
-        if isinstance(__value, str) or isinstance(__value, RiotString):
-            return str(self)==str(__value)
 
-DIGIT       = RiotString(r'\d')
-NON_DIGIT   = RiotString(r'\D')
-ANYTHING    = RiotString(r'.')
-ALPHANUM    = RiotString(r'\w')
-NON_ALPHANUM= RiotString(r'\W')
-SPACE       = RiotString(r'\s')
-NON_SPACE   = RiotString(r'\S')
-BOUNDARY    = RiotString(r'\b')
-NON_BOUNDARY= RiotString(r'\B')
+
+    # def __eq__(self, __value: object) -> bool:
+    #     if isinstance(__value, str) or isinstance(__value, RiotString):
+    #         return str(self)==str(__value)
+
+def one_or_more(rs:RiotString):
+    return rs.one_or_more()
+
+
+DIGIT       = RiotString(r'\d', unit=True)
+NON_DIGIT   = RiotString(r'\D', unit=True)
+ANYTHING    = RiotString(r'.', unit=True)
+ALPHANUM    = RiotString(r'\w', unit=True)
+NON_ALPHANUM= RiotString(r'\W', unit=True)
+SPACE       = RiotString(r'\s', unit=True)
+NON_SPACE   = RiotString(r'\S', unit=True)
+BOUNDARY    = RiotString(r'\b', unit=True)
+NON_BOUNDARY= RiotString(r'\B', unit=True)
+DOT         = RiotString(r'\.', unit=True)
--- tests/test_symbols.py
+++ tests/test_symbols.py
@@ -4,25 +4,28 @@ import unittest
 class LiteralsStringValueTests(unittest.TestCase):
     
     def test_digit(self):
-        self.assertEqual(DIGIT, r'\d')
-        self.assertEqual(NON_DIGIT, r'\D')
+        self.assertEqual(str(DIGIT), r'\d')
+        self.assertEqual(str(NON_DIGIT), r'\D')
     
     def test_anything(self):
-        self.assertEqual(ANYTHING, r'.')
+        self.assertEqual(str(ANYTHING), r'.')
     
     def test_alphanum(self):
-        self.assertEqual(ALPHANUM, r'\w')
-        self.assertEqual(NON_ALPHANUM, r'\W')
+        self.assertEqual(str(ALPHANUM), r'\w')
+        self.assertEqual(str(NON_ALPHANUM), r'\W')
         
     def test_space(self):
-        self.assertEqual(SPACE, r'\s')
-        self.assertEqual(NON_SPACE, r'\S')
+        self.assertEqual(str(SPACE), r'\s')
+        self.assertEqual(str(NON_SPACE), r'\S')
     
     def test_boundary(self):
-        self.assertEqual(BOUNDARY, r'\b')
-        self.assertEqual(NON_BOUNDARY, r'\B')
+        self.assertEqual(str(BOUNDARY), r'\b')
+        self.assertEqual(str(NON_BOUNDARY), r'\B')
+    
+    def test_dot(self):
+        self.assertEqual(str(DOT), r'\.')
     
     def test_custom_string(self):
         words = ['hello','there', ',', ' ', 'general', 'kenobi']
         for w in words:
-            self.assertEqual(RiotString(w), w)
+            self.assertEqual(str(RiotString(w)), w)
