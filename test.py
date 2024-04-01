9815eb57c2d77c24706218850a0b9f4f94c3f298
Rahul <rahukand@gmail.com>
2024-04-01 01:53:01 -0700

    Added github actions and unittest

--- test.py
+++
@@ -1,3 +0,0 @@
-from regex_riot_rk22000 import example
-
-print(example.add_one(5))
---
+++ tests/test1.py
@@ -0,0 +1,11 @@
+from src.regex_riot_rk22000.example import add_one
+import unittest
+
+class Test1(unittest.TestCase):
+    
+    def test_t1(self):
+        self.assertEqual(7, add_one(5))
+
+
+if __name__=='__main__':
+    unittest.main()
