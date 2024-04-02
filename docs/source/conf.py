05deea66317691faa4987ed9a9721606461f8a17
Rahul <rahukand@gmail.com>
2024-04-01 21:07:57 -0700

    Created a simple sphinx site

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RegexRiot'
+copyright = '2024, Rahul Kandekar'
+author = 'Rahul Kandekar'
+release = '0.0.1'
+
+import sys
+import os
+sys.path.append(os.path.join('..','src'))
+sys.path.append(os.path.join('..', 'tests'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. RegexRiot documentation master file, created by
+   sphinx-quickstart on Mon Apr  1 18:32:34 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to RegexRiot's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
